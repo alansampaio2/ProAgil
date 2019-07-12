3aa95ca252e8d926078cd1fd4bb41ba5caad4862
Alan Mangueira Sampaio <alanmangueira@gmail.com>
2019-07-12 17:50:00 -0300

    Usando FormBuilder Service

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // NGX BOOTSTRAP
 import { ModalModule, TooltipModule, BsDropdownModule } from 'ngx-bootstrap';
@@ -27,7 +27,8 @@ import { EventoService } from './_services/evento.service';
       FormsModule,
       BsDropdownModule.forRoot(),
       TooltipModule.forRoot(),
-      ModalModule.forRoot()
+      ModalModule.forRoot(),
+      ReactiveFormsModule
    ],
    providers: [
       EventoService
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../_services/evento.service';
 import { Evento } from '../_models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
@@ -26,13 +27,16 @@ export class EventosComponent implements OnInit {
   imagemMargem = 2;
   mostrarImagem = false;
   modalRef: BsModalRef;
+  registerForm: FormGroup; // PARA USAR O FORMULÁRIO (será o nome do formulario)
 
   constructor(
     private eventoService: EventoService,
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private fb: FormBuilder
     ) { }
 
   ngOnInit() {    // O ngOnInit É CHAMADO ANTES DE CARREGAR O HTML
+    this.validation();
     this.getEventos();
   }
 
@@ -59,6 +63,22 @@ export class EventosComponent implements OnInit {
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
+  }
+
+  salvarAlteracao() {
+
+  }
+
+  validation() {
+    this.registerForm = this.fb.group({ // OBSERVAÇÃO:  TODOS OS CAMPOS DO FORMULÁRIO TEM QUE ESTÁ REPRESENTADOS AQUI
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      imagemURL: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
 
 }
